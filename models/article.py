bb7d191b5795b2d2c52a07c2921615ad5f0ba99a
Hamdiibrahim <yusufhamdi137@gmail.com>
2024-12-14 12:11:09 +0300

    article instansiation

--- models/article.py
+++ models/article.py
@@ -1,5 +1,9 @@
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
+    def __init__(self, id:int, title:str, content:str, author_id:int, magazine_id:int):
+        if not isinstance(title,str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters.")
+        if not isinstance(content,str) or len(content) == 0:
+            raise ValueError("Content cannot be empty.")
         self.id = id
         self.title = title
         self.content = content
